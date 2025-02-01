57a1b8c6596857fb9084deb5adcb97508e898340
Abhinav Verma <legendav@kali>
2025-02-01 20:53:13 +0530

    subfinal_product

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import InputBox from './components/InputBox.jsx'
 import './App.css'
-
+import useCurrInfo from './hooks/useCurr.js'
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount , setAmount] = useState(0)
+  const [from , setFrom] = useState("usd")
+  const [to , setTo] = useState("inr")
+  const [convertedAmount , setconvertedAmount] = useState(0)
 
+  const currencyInfo = useCurrInfo(from)
+  const options = Object.keys(currencyInfo)
+  console.log(options)
+  const swap = ()=>{
+    setFrom(to)
+    setTo(from)
+    setconvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+  const converter = ()=>{
+    setconvertedAmount(amount*currencyInfo[to])
+  }
+  const BackgroundImage = 'https://images.pexels.com/photos/7233356/pexels-photo-7233356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+            backgroundImage: `url('${BackgroundImage}')`,
+        }}
+    >
+        <div className="w-full">
+            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        converter()
+                    }}
+                >
+                    <div className="w-full mb-1">
+                        <InputBox
+                            label="From"
+                            amount = {amount}
+                            currencyOption={options}
+                            onCurrencyChange={(currency)=>setAmount(amount)}
+                            selectCurrency={from}
+                            onAmountChange={(amount)=>setAmount(amount)}
+                        />
+                    </div>
+                    <div className="relative w-full h-0.5">
+                        <button
+                            type="button"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            onClick={swap}
+                        >
+                            swap
+                        </button>
+                    </div>
+                    <div className="w-full mt-1 mb-4">
+                        <InputBox
+                            label="To"
+                            amount = {convertedAmount}
+                            currencyOption={options}
+                            onCurrencyChange={(currency)=>setTo(currency)}
+                            selectCurrency={from}
+                        />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {from.toUpperCase} to {to.toUpperCase}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
 }
 
 export default App
